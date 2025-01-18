8d3f6dee927283b30eac8fe4981bc9a6f17ccb68
idosarig <idosarig10@gmail.com>
2025-01-18 23:23:28 +0200

    Set deployment base path to '/paper-finder'

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/paper-finder',
     plugins: [react(), svgr()],
 })
